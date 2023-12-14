d00e4522fe8b0e7040d5cedd4eeb8cb08950e00f
Kevin Levasseur <kevin_levasseur@telus.net>
2023-12-14 15:39:11 -0800

    initial commit

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// function takes two arrays and returns true or false, based on a perfect match
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) { //checks if both arrays are equal length. if one is longer it ends the function with a return of false.
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) { //iterates through the arrays checking each coresponding value to eachother for inequality.
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true; //if no issues are found (different lengths or values) a value of true is returned
+};
+
+// test code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
+assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, "Q", null], [1, "Q", null]), true);
