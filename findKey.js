ae42c2f9a736f55649b70398c204ac03f3b852c1
Kevin Levasseur <kevin_levasseur@telus.net>
2023-12-19 16:57:35 -0800

    intial commit

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// FUNCTION IS HERE
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+// TEST CODE
+const testObject = {
+  "Blue Hill": { stars: 1, style: "diner"},
+  "Akaleri":   { stars: 3, style: "bakery"},
+  "noma":      { stars: 2, style: "cafe"},
+  "elBulli":   { stars: 3, style: "mexican"},
+  "Ora":       { stars: 2, style: "greek"},
+  "Akelarre":  { stars: 3, style: "food truck"}
+};
+
+assertEqual(findKey(testObject, x => x.stars === 2), "noma");
+assertEqual(findKey(testObject, x => x.stars === 4), undefined);
+assertEqual(findKey(testObject, x => x.style === "greek"), "Ora");
