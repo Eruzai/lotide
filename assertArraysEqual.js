e40525e925703f0aa1f550c5dd4659894d628c8d
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-15 15:13:06 -0800

    refactored code to separate tests

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-// eqArrays function takes two arrays and returns true or false, based on a perfect match
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { //checks if both arrays are equal length. if one is longer it ends the function with a return of false.
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) { //iterates through the arrays checking each coresponding value to eachother for inequality.
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true; //if no issues are found (different lengths or values) a value of true is returned
-};
+const eqArrays = require('./eqArrays');
 
 // assertArraysEqual function takes two arrays and outputs an appropriate message to the console
 const assertArraysEqual = function(arr1, arr2) {
@@ -22,5 +9,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual(["a", 2, 3], [1, " ", 3]);
+module.exports = assertArraysEqual;
