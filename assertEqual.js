ca1f4168e2b2a1d2e8259f504a7d410c658bcb1c
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-15 14:47:48 -0800

    refactored files to seperate test code and use require call

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -7,11 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
-assertEqual("hi", "hello");
-assertEqual(5, 52);
-assertEqual(5, "hi");
-assertEqual(0, null);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,19 +1,6 @@
-//USED TO TEST FOR EXPECTED VALUES
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 //RETURNS THE HEAD (FIRST ELEMENT) OF AN ARRAY
 const head = function(arr) {
   return arr[0];
 };
 
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["one"]), "one");
-assertEqual(head([]), undefined);
+module.exports = head;
